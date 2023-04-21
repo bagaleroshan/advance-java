1e0ce0dff71d0bcc013550c33f54c1afe397aaaa
bagaleroshan <roshanbagale21@gmail.com>
2023-04-21 10:53:36 +0545

    calculate

--- calculate.java
+++ calculate.java
@@ -2,27 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
  class calculate extends JFrame implements ActionListener{
-    JLabel num1,num2;
+    JLabel num1;
+    JLabel num2;
     JButton Calculate;
-    JTextField fnum1,fnum2;
+    JTextField fnum1;
+    JTextField fnum2;
     JRadioButton r1,r2;
 
     calculate(){
-num1=new JLabel("number1");
-num2=new JLabel("number2");
+num1=new JLabel("num1");
+num2=new JLabel("num2");
 r1=new JRadioButton("add");
 r2=new JRadioButton("prod");
 fnum1=new JTextField(20);
 fnum2=new JTextField(20);
-r1.addActionListener(this);
-r2.addActionListener(this);
+//r1.addActionListener(this);
+//r2.addActionListener(this);
 Calculate =new JButton("calculate");
 setSize(400,500);
 setVisible(true);
 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 setLayout(new FlowLayout());
-add(num1);
-add(num2);
+add(num1);add(fnum1);
+add(num2);add(fnum2);
 add(r1);
 add(r2);
 add(Calculate);
@@ -32,11 +34,11 @@ Calculate.addActionListener(this);
     public void actionPerformed(ActionEvent ee){
       int num1=Integer.parseInt(fnum1.getText());
         int num2=Integer.parseInt(fnum2.getText());
-        if(r2.isSelected()){
-int sum=num2+num2;
+        if(r1.isSelected()){
+int sum=num1+num2;
 System.out.println("sum is"+sum);
         }
-        else if(r1.isSelected()){
+        else if(r2.isSelected()){
             int product=num1*num2;
             System.out.println("product is"+product);
         }
